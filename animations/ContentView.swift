4cc766967f2b5499dd7be376531f061da3861c88
weru <onewesh@gmail.com>
2020-02-28 15:12:49 +0300

    Initial Commit

---
+++ animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  animations
+//
+//  Created by dan on 2/28/20.
+//  Copyright © 2020 dan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
